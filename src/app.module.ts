ad234d9367b681e47e49484121a8fda6e95d6cf6
Kimbohy <lovabarwijons@gmail.com>
2025-02-12 12:15:24 +0300

    add authentication and user management modules with basic services and controllers
    (sign up OK)

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BooksModule } from './books/books.module';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [BooksModule],
+  imports: [BooksModule, UsersModule, AuthModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/books/books.module.ts
+++ src/books/books.module.ts
@@ -6,4 +6,22 @@ import { BooksService } from './books.service';
   controllers: [BooksController],
   providers: [BooksService],
 })
-export class BooksModule {}
+export class BooksModule {
+  private users = [
+    {
+      id: 0,
+      name: 'Kim',
+      email: 'kim@gmail.com',
+      hashedPassword: 'SKGKE23KDK3KGK55K3KF334KV',
+      createdAt: new Date('2024-05-12'),
+    },
+  ];
+
+  getUsers(id?: string, email?: string, name?: string, createdAt?: Date) {
+    // todo: implement this function properly
+    if (id) {
+      return this.users.filter((user) => {});
+    }
+  }
+}
+ 
