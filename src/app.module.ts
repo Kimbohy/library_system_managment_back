36c7f965930a88dfa107b1c3f4f6e55685feebb1
Kimbohy <lovabarwijons@gmail.com>
2025-03-12 22:44:01 +0300

    feat: add books module with controller and service; include unit tests for both

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,9 +9,11 @@ import { UsersController } from './users/users.controller';
 import { UsersService } from './users/users.service';
 import { UsersModule } from './users/users.module';
 import { AdminModule } from './admin/admin.module';
+import { BooksService } from './books/books.service';
+import { BooksModule } from './books/books.module';
 
 @Module({
-  imports: [AuthModule, PrismaModule, UsersModule, AdminModule],
+  imports: [AuthModule, PrismaModule, UsersModule, AdminModule, BooksModule],
   controllers: [AppController, UsersController],
   providers: [
     AppService,
@@ -20,6 +22,7 @@ import { AdminModule } from './admin/admin.module';
       useClass: AtGuard,
     },
     UsersService,
+    BooksService,
   ],
 })
 export class AppModule {}
